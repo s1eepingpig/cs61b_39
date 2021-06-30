d44a47cb5f6a9a9358a2cbfefb8175e924493cc5
Ryn <you@example.com>
2021-06-30 17:34:59 +0800

    update project1A

--- proj1a/test.java
+++ proj1a/test.java
@@ -2,13 +2,11 @@ public class test {
     public static void main(String[] args) {
         LinkedListDeque<Integer> LinkedListDeque = new LinkedListDeque<>();
         LinkedListDeque.isEmpty();
+        LinkedListDeque.addFirst(0);
         LinkedListDeque.addFirst(1);
         LinkedListDeque.addFirst(2);
         LinkedListDeque.addFirst(3);
-        LinkedListDeque.addFirst(4);
-        LinkedListDeque.addFirst(5);
-        LinkedListDeque.isEmpty();
-        LinkedListDeque.printDeque();
+
         Integer integer = LinkedListDeque.removeLast();
         LinkedListDeque.printDeque();
 
